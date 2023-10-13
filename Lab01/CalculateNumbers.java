527a1e008cda3cd6083e32f61a0f0b25bb2b2c76
hiewbt <hieubt.bk66@gmail.com>
2023-10-13 21:38:37 +0700

    Create folder Lab01

--- Lab01/CalculateNumbers.java
+++ Lab01/CalculateNumbers.java
@@ -1,3 +1,4 @@
+package Lab01;
 import javax.swing.JOptionPane;
 public class CalculateNumbers {
     public static void main(String[] args){
--- Lab01/Equation.java
+++ Lab01/Equation.java
@@ -1,3 +1,4 @@
+package Lab01;
 import java.util.Scanner;
 import java.lang.Math;
 public class Equation {
