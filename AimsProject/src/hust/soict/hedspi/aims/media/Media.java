714c306dd5da326a9cc8745a5dc2c649976a24c0
hiewbt <hieubt.bk66@gmail.com>
2023-11-26 22:15:16 +0700

    complete section 17

--- AimsProject/src/hust/soict/hedspi/aims/media/Media.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Media.java
@@ -1,8 +1,12 @@
 package hust.soict.hedspi.aims.media;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public abstract class Media {
+    public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
+    public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
+
     private int id;
     private String title;
     private String category;
