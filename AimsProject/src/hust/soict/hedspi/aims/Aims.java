0b63292876d30c180dfee0252defd137b571b88c
hiewbt <hieubt.bk66@gmail.com>
2023-12-09 21:08:31 +0700

    update class diagram

--- AimsProject/src/hust/soict/hedspi/aims/Aims.java
+++ AimsProject/src/hust/soict/hedspi/aims/Aims.java
@@ -4,8 +4,6 @@ import hust.soict.hedspi.aims.cart.Cart;
 import hust.soict.hedspi.aims.media.*;
 import hust.soict.hedspi.store.Store;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 
